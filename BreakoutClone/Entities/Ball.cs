4292ef1b9d76a0ca73631ca0f2327ee54b659653
jkendall327 <jkendall3096@gmail.com>
2020-08-05 13:32:43 +0100

    Refactored CheckForPaddle() significantly. No more ugly switch statement.

--- BreakoutClone/Entities/Ball.cs
+++ BreakoutClone/Entities/Ball.cs
@@ -71,26 +71,113 @@ namespace BreakoutClone
 
             CheckForWalls();
 
-            // Some basic checks so it's not checking for collision literally every frame.
-            // Bottom half of screen, check for paddle. Top half, check for bricks.
+            CheckForPaddle();
 
-            if (Position.Y > 200)
-            {
-                CheckForPaddle();
-            }
-
-            if (Position.Y < 200)
-            {
-                CheckForBrick(wall);
-            }
+            CheckForBrick(wall);
 
             CheckForItem(items);
 
         }
 
+        private void ChangePosition()
+        {
+            Position.X += (float)XVelocity;
+            Position.Y += (float)YVelocity;
+        }
+
+        private void CheckForWalls()
+        {
+            // Velocity multiplied by -0.9 to slow down ball on wall hit. 
+
+            Double WallSpeedModifier = -0.9;
+
+            //Position.X = Helper.Clamp(Position.X, 0, Breakout.ScreenSize.X - Width);
+            //Position.Y = Helper.Clamp(Position.Y, 0, Breakout.ScreenSize.Y);
+
+            // Hit left wall
+            if (Position.X < 0)
+            {
+                Position.X = 0;
+                XVelocity *= WallSpeedModifier;
+            }
+
+            // Hit right wall
+            if (Position.X + Width > Breakout.ScreenSize.X)
+            {
+                Position.X = Breakout.ScreenSize.X - Width;
+                XVelocity *= WallSpeedModifier;
+            }
+
+            // Hit top of screen
+
+            if (Position.Y < 0)
+            {
+                Position.Y = 0;
+                YVelocity *= WallSpeedModifier;
+            }
+
+            // Hit bottom of screen
+            if (Position.Y + Height > Breakout.ScreenSize.Y)
+            {
+                Reset();
+            }
+        }
+
+        private int GetOffset(Rectangle rectangle)
+        {
+            float PointOfContactOnPaddle = rectangle.X + rectangle.Width - Position.X + Width / 2;
+
+            int offset = Convert.ToInt32((PaddleHitbox.Width - PointOfContactOnPaddle)) / 5;
+
+            return Helper.Clamp(offset, 0, int.MaxValue);
+        }
+
+        private void CheckForPaddle()
+        {
+            Rectangle ballHitbox = GetCurrentHitbox();
+
+            if (ballHitbox.Intersects(PaddleHitbox) == false)
+            {
+                return;
+            }
+
+            int offset = GetOffset(PaddleHitbox);
+
+            // TODO: magic number
+            offset -= 6;
+
+            XVelocity = offset;
+
+            YVelocity *= -1;
+        }
+
+        private void CheckForBrick(Wall wall)
+        {
+            Rectangle ballHitbox = GetCurrentHitbox();
+
+            foreach (Brick brick in wall.BrickWall)
+            {
+                if (brick.IsAlive && ballHitbox.Intersects(brick.Hitbox))
+                {
+                    brick.IsAlive = false;
+
+                    wall.BricksLeft -= 1;
+                    Console.WriteLine(wall.BricksLeft);
+
+                    // Invert direction and increase speed.
+                    YVelocity *= -1.3;
+
+                    // Breaking out of the loop is what stops the ball
+                    // from destroying multiple bricks at once.
+                    // TODO: idea for a powerup?
+                    break;
+                }
+            }
+        }
+
         private void CheckForItem(List<Item> items)
         {
-            Rectangle ballHitbox = new Rectangle(Position.ToPoint(), new Point(Width, Height));
+            Rectangle ballHitbox = GetCurrentHitbox();
 
             foreach (Item item in items)
             {
@@ -102,6 +189,11 @@ namespace BreakoutClone
 
             }
 
+        }
+
+        private Rectangle GetCurrentHitbox()
+        {
+            return new Rectangle(Position.ToPoint(), new Point(Width, Height));
         }
 
         public void Reset()
@@ -135,134 +227,6 @@ namespace BreakoutClone
                 YVelocity = -3;
             }
 
-        }
-
-        private void ChangePosition()
-        {
-            Position.X += (float)XVelocity;
-            Position.Y += (float)YVelocity;
-        }
-
-        private void CheckForBrick(Wall wall)
-        {
-            Rectangle ballHitbox = new Rectangle(Position.ToPoint(), new Point(Width, Height));
-
-            foreach (Brick brick in wall.BrickWall)
-            {
-                if (brick.IsAlive && ballHitbox.Intersects(brick.Hitbox))
-                {
-                    brick.IsAlive = false;
-
-                    wall.BricksLeft -= 1;
-                    Console.WriteLine(wall.BricksLeft);
-
-                    // Invert direction and increase speed.
-                    YVelocity *= -1.3;
-
-                    // Breaking out of the loop is what stops the ball
-                    // from destroying multiple bricks at once.
-                    // TODO: idea for a powerup?
-                    break;
-                }
-            }
-        }
-
-        private void CheckForPaddle()
-        {
-            Rectangle ballHitbox = new Rectangle(Position.ToPoint(), new Point(Width, Height));
-
-            if (ballHitbox.Intersects(PaddleHitbox))
-            {
-                float PointOfContactOnPaddle = PaddleHitbox.X + PaddleHitbox.Width - Position.X + Width / 2;
-
-                int offset = Convert.ToInt32((PaddleHitbox.Width - PointOfContactOnPaddle));
-                offset /= 5;
-
-                if (offset < 0)
-                {
-                    offset = 0;
-                }
-
-                switch (offset)
-                {
-                    case 0:
-                        XVelocity = -6;
-                        break;
-                    case 1:
-                        XVelocity = -5;
-                        break;
-                    case 2:
-                        XVelocity = -4;
-                        break;
-                    case 3:
-                        XVelocity = -3;
-                        break;
-                    case 4:
-                        XVelocity = -2;
-                        break;
-                    case 5:
-                        XVelocity = -1;
-                        break;
-                    case 6:
-                        XVelocity = 1;
-                        break;
-                    case 7:
-                        XVelocity = 2;
-                        break;
-                    case 8:
-                        XVelocity = 3;
-                        break;
-                    case 9:
-                        XVelocity = 4;
-                        break;
-                    case 10:
-                        XVelocity = 5;
-                        break;
-                    default:
-                        XVelocity = 6;
-                        break;
-                }
-
-                YVelocity *= -1;
-                Position.Y = PaddleHitbox.Y - Height + 1;
-            }
-        }
-
-        private void CheckForWalls()
-        {
-            // Velocity multiplied by -0.9 to slow down ball on wall hit. 
-
-            Double WallSpeedModifier = -0.9;
-
-            // Hit left wall
-            if (Position.X < 0)
-            {
-                Position.X = 0;
-                XVelocity *= WallSpeedModifier;
-            }
-
-            // Hit right wall
-            if (Position.X + Width > Breakout.ScreenSize.X)
-            {
-                Position.X = Breakout.ScreenSize.X - Width;
-                XVelocity *= WallSpeedModifier;
-            }
-
-            // Hit top of screen
-
-            if (Position.Y < 0)
-            {
-                Position.Y = 0;
-                YVelocity *= WallSpeedModifier;
-            }
-
-            // Hit bottom of screen
-            if (Position.Y + Height > Breakout.ScreenSize.Y)
-            {
-                Position.X = Breakout.ScreenSize.X / 2;
-                Position.Y = Breakout.ScreenSize.Y / 2;
-                IsActive = false;
-            }
         }
 
         public void Draw(SpriteBatch spritebatch)
