0e6a7cc4ac5b394a9cceb6de2c2d50e3c1061c6d
jkendall327 <jkendall3096@gmail.com>
2020-08-06 14:40:05 +0100

    Making master class for catching input that fires events when key is pressed.

---
+++ BreakoutClone/Screens/InputHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakoutClone.Screens
+{
+    class InputHandler
+    {
+        private KeyboardState oldKeyboardState;
+        private KeyboardState keyboardState;
+
+        public event EventHandler<KeyboardEventArgs> keyPressed;
+
+        public class KeyboardEventArgs
+        {
+        }
+
+        public InputHandler()
+        {
+
+        }
+
+        public void Update()
+        {
+            keyboardState = Keyboard.GetState();
+
+            // No keys were pressed this frame or last frame, meaning there can be no button presses.
+
+            if (keyboardState.GetPressedKeys().Length == 0 && oldKeyboardState == keyboardState)
+            {
+                return;
+            }
+
+
+            oldKeyboardState = keyboardState;
+        }
+
+
+    }
+}
